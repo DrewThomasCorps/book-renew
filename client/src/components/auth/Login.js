1e823413bc1c4d850fa223a5b2a2209ed755f8f3
Alan Bauer <adbauer@bsu.edu>
2019-10-09 17:59:25 -0400

    add onChange and onSubmit handlers for Login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,13 +1,36 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Link} from 'react-router-dom';
 
 const Login = () => {
+    const [loginInformation, setLoginInformation] = useState({
+        email: '',
+        password: ''
+    });
+
+    const {email, password} = loginInformation;
+
+    const handleInputChange = e => setLoginInformation({ ...loginInformation, [e.target.name] : e.target.value});
+
+    const handleLoginSubmit = async e => {
+        e.preventDefault();
+
+        if(email === '' || password === ''){
+            console.log("Please enter your email and password");
+        } else {
+            const signInAttempt = {
+                email,
+                password
+            };
+            console.log(signInAttempt);
+        }
+    };
+
     return(
         <Fragment>
             <h1>Sign In</h1>
-            <form>
-                <input type={"email"} placeholder={"Email Address"} />
-                <input type={"password"} placeholder={"Password"} />
+            <form onSubmit={handleLoginSubmit}>
+                <input type={"email"} name={"email"} value={email} onChange={handleInputChange} placeholder={"Email Address"} />
+                <input type={"password"} name={"password"} value={password} onChange={handleInputChange}  placeholder={"Password"} />
                 <input type={"submit"} value={"Sign In"} />
             </form>
             <div>
