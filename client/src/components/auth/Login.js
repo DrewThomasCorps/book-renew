7fd438a8eb97579cf58feab722fc8bad63a8423e
Tristan Oshier <tsoshier@bsu.edu>
2019-10-09 18:58:47 -0400

    refactor form classes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -27,14 +27,14 @@ const Login = () => {
 
     return(
         <Fragment>
-            <h1>Sign In</h1>
-            <form onSubmit={handleLoginSubmit}>
-                <input type={"email"} name={"email"} value={email} onChange={handleInputChange} placeholder={"Email Address"} />
-                <input type={"password"} name={"password"} value={password} onChange={handleInputChange}  placeholder={"Password"} />
-                <input type={"submit"} value={"Sign In"} />
+            <h1 className={"form-heading"}>Sign In</h1>
+            <form className={"form form-login"} onSubmit={handleLoginSubmit}>
+                <input className={"form-input"} type={"email"} name={"email"} value={email} onChange={handleInputChange} placeholder={"Email Address"} />
+                <input className={"form-input"} type={"password"} name={"password"} value={password} onChange={handleInputChange}  placeholder={"Password"} />
+                <input className={"btn btn-primary"} type={"submit"} value={"Sign In"} />
             </form>
-            <div>
-                <p>Don't have an account? <Link to={"/registration"}>Sign up here</Link></p>
+            <div className={"info-box"}>
+                <p className={"info-box-text"}>Don't have an account? <Link className={"info-box-link"} to={"/registration"}>Sign up here</Link></p>
             </div>
         </Fragment>
     )
