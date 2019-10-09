e3b6b44dfae5c300e3e5c4f782fef2dec109e2bd
Alan Bauer <adbauer@bsu.edu>
2019-10-09 17:41:12 -0400

    add handlers for onSubmit and onChange to Registration

--- client/src/components/auth/Registration.js
+++ client/src/components/auth/Registration.js
@@ -1,13 +1,37 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Registration = () => {
+    const [accountData, setAccountData] = useState({
+        email: '',
+        password: '',
+        passwordConfirmation: ''
+    });
+
+    const {email, password, passwordConfirmation} = accountData;
+
+    const handleFormSubmit = async e => {
+        e.preventDefault();
+
+        if(password !== passwordConfirmation){
+            console.log("Password mismatch");
+        } else {
+            const newAccount = {
+                email,
+                password
+            };
+            console.log(newAccount)
+        }
+    };
+
+    const handleInputChange = e => setAccountData({ ...accountData, [e.target.name] : e.target.value});
+
     return(
         <Fragment>
             <h1>Account Creation</h1>
-            <form>
-                <input type={"email"} placeholder={"Email Address"} />
-                <input type={"password"} placeholder={"Password"} />
-                <input type={"password"} placeholder={"Password Confirmation"} />
+            <form onSubmit={handleFormSubmit}>
+                <input type={"email"} onChange={handleInputChange} name="email" value={email} placeholder={"Email Address"} />
+                <input type={"password"} onChange={handleInputChange} name="password"  value={password} placeholder={"Password"} />
+                <input type={"password"} onChange={handleInputChange} name="passwordConfirmation"  value={passwordConfirmation} placeholder={"Password Confirmation"} />
                 <input type={"submit"} value={"Sign Up"} />
             </form>
         </Fragment>
