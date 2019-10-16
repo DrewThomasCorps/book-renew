6ac9130da47d6b828e5eb43733374f32aaf61a74
Tristan Oshier <tsoshier@bsu.edu>
2019-10-16 15:17:57 -0400

    refactor Registration.js form

--- client/src/components/auth/Registration.js
+++ client/src/components/auth/Registration.js
@@ -32,8 +32,8 @@ const Registration = ({registerUser, isAuthenticated}) => {
 
     return(
         <Fragment>
-            <h1 className={"form-heading"}>Account Creation</h1>
             <form className={"form form-registration"} onSubmit={handleAccountCreationSubmit}>
+                <h1 className={"form-heading"}>Account Creation</h1>
                 <input className={"form-input"} type={"text"} onChange={handleInputChange} name="username" value={username} placeholder={"Username"} />
                 <input className={"form-input"} type={"email"} onChange={handleInputChange} name="email" value={email} placeholder={"Email Address"} />
                 <input className={"form-input"} type={"password"} onChange={handleInputChange} name="password"  value={password} placeholder={"Password"} />
@@ -41,10 +41,10 @@ const Registration = ({registerUser, isAuthenticated}) => {
                        name="passwordConfirmation"  value={passwordConfirmation} placeholder={"Password Confirmation"}
                 />
                 <input className={"btn btn-primary"} type={"submit"} value={"Sign Up"} />
+                <div className={"info-box"}>
+                    <p className={"info-box-text"}>Already have an account? <Link className={"info-box-link"} to={"/"}>Sign in</Link> here.</p>
+                </div>
             </form>
-            <div className={"info-box"}>
-                <p className={"info-box-text"}>Already have an account? <Link className={"info-box-link"} to={"/"}>Sign in</Link> here.</p>
-            </div>
         </Fragment>
     )
 };
