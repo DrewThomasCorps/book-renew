0e0c703942834c201c823db8bf9d8dd1c333af69
Drew Thomas <amthomas12@bsu.edu>
2019-10-15 22:34:22 -0400

    Add WebSecurity to get access to Bcrypt

--- book_renew_backend/src/test/java/com/bookrenew/api/UserTests.java
+++ book_renew_backend/src/test/java/com/bookrenew/api/UserTests.java
@@ -24,29 +24,50 @@ import java.io.IOException;
 public class UserTests {
 
     private RestTemplate restTemplate = new RestTemplate();
-
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final String baseUrl = "http://localhost:8080/";
+    private JsonNode responseRoot;
 
     @Test
-    public void stage1_shouldCreateUser() throws JSONException, IOException {
+    public void testCRUD() throws JSONException, IOException {
+        this.testCreateUser();
+    }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+    private void testCreateUser() throws JSONException, IOException {
+        JSONObject userJsonObject = this.buildUserJsonObject();
+        HttpEntity<String> request = this.buildRequest(userJsonObject);
+        this.sendPostRequest(request);
+        this.testPostResponseContainsCorrectData();
+    }
+
+    private JSONObject buildUserJsonObject() throws JSONException {
         JSONObject userJsonObject = new JSONObject();
         userJsonObject.put("name", "testUser");
         userJsonObject.put("email", "test@test.com");
         userJsonObject.put("username", "testUsername");
         userJsonObject.put("password", "testPassword");
-
-        HttpEntity<String> request = new HttpEntity<String>(userJsonObject.toString(), headers);
-
-        String userResultsAsJsonString = restTemplate.postForObject("http://localhost:8080/users", request, String.class);
-        JsonNode root = objectMapper.readTree(userResultsAsJsonString);
-
-        Assert.assertNotNull(root);
-        Assert.assertNotNull(root.path("name").asText());
+        return userJsonObject;
     }
 
+    private HttpEntity<String> buildRequest(JSONObject data) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity<>(data.toString(), headers);
+    }
+
+    private void sendPostRequest(HttpEntity<String> request) throws IOException {
+        String userResultsAsJsonString = restTemplate.postForObject(baseUrl + "users", request, String.class);
+        responseRoot = objectMapper.readTree(userResultsAsJsonString);
+    }
+
+    private void testPostResponseContainsCorrectData() {
+        Assert.assertNotNull(responseRoot);
+        Assert.assertEquals("testUser", responseRoot.path("name").asText());
+        Assert.assertEquals("test@test.com", responseRoot.path("email").asText());
+        Assert.assertEquals("testUsername", responseRoot.path("username").asText());
+        Assert.assertEquals("", responseRoot.path("password").asText());
+        Assert.assertNotNull(responseRoot.path("id"));
+    }
 
 
 }
