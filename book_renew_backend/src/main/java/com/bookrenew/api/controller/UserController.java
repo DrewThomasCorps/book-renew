dd23e2cc5f62a1e4b8a64eece83185d66a8e33aa
Drew Thomas <amthomas12@bsu.edu>
2019-10-19 22:38:12 -0400

    Refactor and Add Unit tests and remove unused routes in controller

--- book_renew_backend/src/main/java/com/bookrenew/api/controller/UserController.java
+++ book_renew_backend/src/main/java/com/bookrenew/api/controller/UserController.java
@@ -2,11 +2,10 @@ package com.bookrenew.api.controller;
 
 import com.bookrenew.api.entity.User;
 import com.bookrenew.api.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -21,29 +20,17 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping(produces = {"application/json"})
-    public Map<String, Iterable<User>> findAll() {
-        Map<String, Iterable<User>> userMap = new HashMap<>();
-        userMap.put("users", repository.findAll());
-        return userMap;
-    }
-
-    @GetMapping(path = "/{id}", produces = {"application/json"})
-    public User find(@PathVariable("id") String id) {
-        Long longId = Long.parseLong(id);
-        return repository.findById(longId).orElse(null);
-    }
-
-    @PostMapping(consumes = "application/json", produces = {"application/json"})
+    @PostMapping(consumes = "application/json", produces = {"application/json"}, path="/register")
     public User create(@RequestBody User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
-    @DeleteMapping(path = "/{id}")
-    public void delete(@PathVariable("id") String id) {
-        Long longId = Long.parseLong(id);
-        User user = repository.findById(longId).orElse(null);
-        repository.delete(user);
+    @GetMapping(produces = {"application/json"}, path = "/authenticated")
+    public User getAuthenticatedUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+        return repository.findByEmail(email);
     }
+
 }
