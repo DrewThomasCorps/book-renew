def4d4f4dae911ee8e11c40de8fc72e92587ef09
Alan Bauer <adbauer@bsu.edu>
2019-10-22 21:03:06 -0400

    Add nav icons

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,19 +1,22 @@
 import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
+import exchangeIcon from "../../exchange-icon.svg";
+import dashboardIcon from "../../dashboard-icon.svg";
+import profileIcon from "../../profile-icon.svg";
 
 const Navigation = () => {
     return(
         <Fragment>
-            <nav className={"br-navbar"}>
+            <nav className={"br-navbar text-center"}>
                 <ul className={"br-navbar-nav"}>
                     <li className={"br-nav-item"}>
-                        <Link className={"br-nav-link"} to={"/dashboard"}>Dashboard</Link>
+                        <Link className={"br-nav-link"} to={"/dashboard"}><img alt={"Dashboard Icon"} className={"br-nav-icon"} src={dashboardIcon} /></Link>
                     </li>
                     <li className={"br-nav-item"}>
-                        <Link className={"br-nav-link"} to={"/trades"}>Trades</Link>
+                        <Link className={"br-nav-link"} to={"/trades"}><img alt={"Trades Icon"} className={"br-nav-icon"} src={exchangeIcon} /></Link>
                     </li>
                     <li className={"br-nav-item"}>
-                        <Link className={"br-nav-link"} to={"/profile"}>Profile</Link>
+                        <Link className={"br-nav-link"} to={"/profile"}><img alt={"Profile Icon"} className={"br-nav-icon"} src={profileIcon} /></Link>
                     </li>
                 </ul>
             </nav>
