ff342826e68601321230e9169782d03f79137ed9
Tristanoshier <tsoshier@bsu.edu>
2019-11-03 20:12:41 -0500

    add classes to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -17,22 +17,22 @@ const Profile = ({ auth: {user,loading}, logoutUser}) => {
                         <section className={"row br-grey"}>
                             <section className={"col-12 content-header"}>
                                 <h1 className={"float-left text-uppercase font-weight-bolder main-text-color header-border"}>Profile</h1>
-                                <a href="#" className={"float-right btn btn-dark my-1 md-3 add-book-btn"} onClick={logoutUser}>Log Out</a>
+                                <a href="#" className={"float-right btn btn-dark my-1 md-3 log-out-btn"} onClick={logoutUser}>Log Out</a>
                             </section>
                             <section className={"col-12 col-md-9 content-panel"}>
                                 <BookForm />
                                 <div className={"row books-container"}>
-                                    <div className={"col-10 book-container br-white left-blue-border shadow"}>
+                                    <div className={"col-10 book-container books-margin br-white left-blue-border shadow"}>
                                         <Books bookType={"library"}/>
                                     </div>
                                 </div>
                                 <div className={"row books-container"}>
-                                    <div className={"col-10 book-container br-white left-blue-border shadow"}>
+                                    <div className={"col-10 book-container books-margin br-white left-blue-border shadow"}>
                                         <Books bookType={"wishlist"}/>
                                     </div>
                                 </div>
                             </section>
-                            <section className={"col-12 col-md-3 content-panel br-bg-blue ice-white-text"}>
+                            <section className={"col-12 col-md-3 content-panel br-bg-blue ice-white-text welcome-box shadow"}>
                                 <h2>Welcome {user.name}</h2>
                                 <h3>{user.email}</h3>
                             </section>
