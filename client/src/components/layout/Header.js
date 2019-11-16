e3dcb48a6ad8cdc923fb012aa743c0f2f8139dac
Alan Bauer <adbauer@bsu.edu>
2019-11-16 12:53:56 -0500

    Add Header component

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Header = ({pageTitle}) => {
+
+    return(
+        <Fragment>
+            <section className={"row"}>
+                <section class={"col-12 order-first content-header"}>
+                    <h1 className={"float-left text-uppercase font-weight-bolder main-text-color header-border"}>{pageTitle}</h1>
+                </section>
+            </section>
+        </Fragment>
+    )
+};
+
+Header.propTypes = {
+    pageTitle: PropTypes.string.isRequired,
+};
+
+export default connect()(Header);
