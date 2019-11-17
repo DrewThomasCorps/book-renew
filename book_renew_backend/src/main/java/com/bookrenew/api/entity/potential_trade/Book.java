13d5e9a4e7fb8678949cee8c4b8f6816fadaa30e
Drew Thomas <amthomas12@bsu.edu>
2019-11-17 16:27:49 -0500

    Add potential trades route and database searcher

---
+++ book_renew_backend/src/main/java/com/bookrenew/api/entity/potential_trade/Book.java
@@ -0,0 +1,27 @@
+package com.bookrenew.api.entity.potential_trade;
+
+class Book {
+    private String title;
+    private String isbn;
+
+    Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
--- book_renew_backend/src/main/java/com/bookrenew/api/repository/UserRepository.java
+++ book_renew_backend/src/main/java/com/bookrenew/api/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.bookrenew.api.repository;
 
 import com.bookrenew.api.entity.User;
+import com.bookrenew.api.entity.potential_trade.PotentialTrade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Query(nativeQuery = true)
+    List<PotentialTrade> findPotentialTrades(Long userId);
 }
