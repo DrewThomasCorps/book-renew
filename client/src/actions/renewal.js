e4c2798883e59a6dc262f4ca661fd3ce049967cb
Alan Bauer <adbauer@bsu.edu>
2019-11-21 16:46:41 -0500

    Add support for renewal type

--- client/src/actions/renewal.js
+++ client/src/actions/renewal.js
@@ -30,6 +30,33 @@ export const getRenewals = () => async dispatch => {
                         "isbn": "789"
                     },
                     "book_user_id" : 8
+                },
+                "status": "active"
+            },
+            {
+                "trader": {
+                    "user" : {
+                        "name" : "Drew Thomas",
+                        "id": 1,
+                        "email" : "amthomas12@bsu.edu"
+                    },
+                    "book": {
+                        "title": "Some Cool Title",
+                        "isbn": "123"
+                    },
+                    "book_user_id" : 4
+                },
+                "tradee": {
+                    "user" : {
+                        "name" : "Drew Thomas",
+                        "id": 2,
+                        "email" : "test@test.edu"
+                    },
+                    "book": {
+                        "title": "Seven Ate Nine!!",
+                        "isbn": "789"
+                    },
+                    "book_user_id" : 8
                 }
             }
         ];
