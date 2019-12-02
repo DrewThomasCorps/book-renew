225922f4e9d550c05f40e167604f71a006d4e4f8
Alan Bauer <adbauer@bsu.edu>
2019-12-02 18:22:38 -0500

    Refactor Renewal component for variable rendering

--- client/src/components/renewal/Renewal.js
+++ client/src/components/renewal/Renewal.js
@@ -4,27 +4,63 @@ import { connect } from 'react-redux';
 import { offerRenewal } from '../../actions/renewal';
 
 const Renewal = ({
+    userId,
     renewal:{trader,tradee,status},
     offerRenewal
 }) => {
+
+    let buttonGroup = <Fragment/>;
+
+    switch(status){
+        case("active"):
+            buttonGroup =
+                <Fragment>
+                    <button>
+                        Complete
+                    </button>
+                    <button>
+                        Cancel
+                    </button>
+                </Fragment>;
+            break;
+        case("pending"):
+            if(tradee.id === userId){
+                buttonGroup = <button>Pending</button>;
+            } else {
+                buttonGroup =
+                    <Fragment>
+                        <button>
+                            Accept
+                        </button>
+                        <button>
+                            Decline
+                        </button>
+                    </Fragment>;
+            }
+            break;
+        default:
+            buttonGroup = <button onClick={() => offerRenewal(trader.id,tradee.id)}>Send Offer</button>
+    }
 
     return (
         <Fragment>
             <article className={"card"}>
                 <div className={"card-body"}>
+                    <small>
+                        { status === "active" ? tradee.user.email : "Library"}
+                    </small>
                     <h5 className={"card-title h6 my-1"}>{trader.book.title}</h5>
                     <small>{trader.book.isbn}</small>
                 </div>
                 <div className={"card-body"}>
+                    <small>
+                        { status === "active" ? trader.user.email : "Wishlist"}
+                    </small>
                     <h5 className={"card-title h6 my-1"}>{tradee.book.title}</h5>
                     <small>{tradee.book.isbn}</small>
                 </div>
                 <div className={"card-body"}>
-                    { !status ?
-                        <button className={"btn btn-primary"} onClick={() => offerRenewal(trader.id,tradee.id)}>
-                            Send Offer
-                        </button> : <Fragment/>
-                    }
+                    { buttonGroup }
                 </div>
             </article>
         </Fragment>
@@ -32,6 +68,7 @@ const Renewal = ({
 };
 
 Renewal.propTypes = {
+    userId: PropTypes.number,
     renewal: PropTypes.object.isRequired,
     offerRenewal: PropTypes.func.isRequired
 };
