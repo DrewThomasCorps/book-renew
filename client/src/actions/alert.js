dc2f71c70e9c43de10f7c48a4dc030811ade185b
Alan Bauer <adbauer@bsu.edu>
2019-12-04 18:58:25 -0500

    Remove uneeded nesting warning while compiling

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -5,13 +5,11 @@ import {
 } from "./types";
 
 export const setAlert = (message, alertType) => dispatch => {
-    {
-        const id = uuid.v4();
-        dispatch({
-            type: SET_ALERT,
-            payload: { message, alertType, id}
-        });
+    const id = uuid.v4();
+    dispatch({
+        type: SET_ALERT,
+        payload: { message, alertType, id}
+    });
 
-        setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id}), 5000);
-    }
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id}), 5000);
 };
