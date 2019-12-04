371d14b6c625091321c1da07562f5f8dd121f237
Alan Bauer <adbauer@bsu.edu>
2019-12-04 18:07:32 -0500

    Fix logout button positioning on mobile

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -7,11 +7,11 @@ const Header = ({ pageTitle, logoutUser}) => {
 
     return(
         <Fragment>
-            <section className={"col-9 order-first content-header"}>
+            <section className={"col-8 order-first content-header"}>
                 <h1 className={"text-uppercase font-weight-bolder main-text-color header-border header-size"}>{pageTitle}</h1>
             </section>
             { pageTitle === "Profile" && (
-                <section className={"col-3"}>
+                <section className={"col-4"}>
                     <button className={"btn btn-dark my-1 md-3 btn-secondary float-right"} onClick={logoutUser}>Log Out</button>
                 </section>
             )}
