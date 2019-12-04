adce40e56ae7e7d8d6ac4b2d2f82e5a7a8ca7ddf
Alan Bauer <adbauer@bsu.edu>
2019-12-04 18:08:03 -0500

    Improve smaller views of Renewal

--- client/src/components/renewal/Renewal.js
+++ client/src/components/renewal/Renewal.js
@@ -11,25 +11,29 @@ const Renewal = ({
 
     return (
         <Fragment>
-            <article className={"row p-3"}>
-                <div className={"col-3 book-card-body book-card-container"}>
-                    <small>
-                        { status === "active" ? trader.user.email : trader.user.name}
-                    </small>
-                    <h5 className={"card-title h6 my-1"}>{trader.book.title}</h5>
-                    <small>{trader.book.isbn}</small>
+            <article className={"row p-3 my-4"}>
+                <div className={"col-12 col-lg-9 col-xl-10 mb-5 mb-lg-0"}>
+                    <div className={"row"}>
+                        <div className={"col-5 book-card-body book-card-container"}>
+                            <small>
+                                { status === "active" ? trader.user.email : trader.user.name}
+                            </small>
+                            <h5 className={"card-title h6 my-1"}>{trader.book.title}</h5>
+                            <small>{trader.book.isbn}</small>
+                        </div>
+                        <div className={"col-2"}>
+                            <img alt={"Renewal Arrows"} className={"br-vCenter"} src={renewalArrows} />
+                        </div>
+                        <div className={"col-5 book-card-body book-card-container"}>
+                            <small>
+                                { status === "active" ? tradee.user.email : tradee.user.name}
+                            </small>
+                            <h5 className={"card-title h6 my-1"}>{tradee.book.title}</h5>
+                            <small>{tradee.book.isbn}</small>
+                        </div>
+                    </div>
                 </div>
-                <div className={"col-3"}>
-                    <img alt={"Renewal Arrows"} className={"br-vCenter"} src={renewalArrows} />
-                </div>
-                <div className={"col-3 book-card-body book-card-container"}>
-                    <small>
-                        { status === "active" ? tradee.user.email : tradee.user.name}
-                    </small>
-                    <h5 className={"card-title h6 my-1"}>{tradee.book.title}</h5>
-                    <small>{tradee.book.isbn}</small>
-                </div>
-                <div className={"col-3"}>
+                <div className={"col-12 col-lg-3 col-xl-2 mt-2 mt-lg-0"}>
                     <RenewalButtonGroup renewal={renewal}/>
                 </div>
             </article>
